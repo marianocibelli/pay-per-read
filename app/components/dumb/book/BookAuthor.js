b5900458a710bda6c10ff1a2f8752bb5b668f1c4
Mariano <mcibelli@garbarino.com>
2017-12-18 15:31:22 -0300

    styled ebook version

--- app/components/dumb/book/BookAuthor.js
+++ app/components/dumb/book/BookAuthor.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const BookAuthor = (props) =>{
       const {author} = props;
-      return (<p className="written-by">{author}</p>)
+      return (<p className="written-by">Written by {author}</p>)
 }
 
 export default BookAuthor
--- app/components/dumb/book/BookVersion.js
+++ app/components/dumb/book/BookVersion.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const BookVersion = (props) =>{
       const {version} = props;
-      return (<p className="book-version">EBOOK VERSION {version}</p>)
+      return (<p className="book-version"><i className="icon-budicon-744 icon" aria-hidden="true"></i> EBOOK VERSION {version}</p>)
 }
 
 export default BookVersion
